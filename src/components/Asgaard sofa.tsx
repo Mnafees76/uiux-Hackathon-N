a53542ec8ea4f96368e1e5609fed4891db8c565f
Muhammad Nafees <165241321+Mnafees76@users.noreply.github.com>
2025-02-11 01:06:55 +0500

    images add

--- src/components/Asgaard sofa.tsx
+++ src/components/Asgaard sofa.tsx
@@ -4,7 +4,7 @@ export default function Asgaardsofa(){
         <main className="flex-col md:flex-row flex justify-between max-w-screen-2xl mx-auto w-full sm:w-full h-full md:h-[500px] bg-[#fff9E5]">
                  {/* Left side */}
                   <div className="mr-0 ml-0">
-                      <Image src={"/imags/p7.png"} width={550} height={550} alt="pic"></Image>
+                      <Image src={"/images/p7.png"} width={550} height={550} alt="pic"></Image>
                   </div>
                    {/* Rihgt side */}
                      <div className="w-full md:w-[500px] md:ml-20 pt-0 md:pt-40 pl-24 md:pl-0 pb-5 md:pb-0">
